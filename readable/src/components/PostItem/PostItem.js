7b1ac91f18f71ed748bbdd22eef4c6e7e1673a2c
Ging Gonzalez <ginggonzalez3@gmail.com>
2017-08-16 20:45:19 -0700

    refactor: Separate PostItem Component from PostsList
    Structure and styles for post items is separated into its own component. This
    way the whole list won't have to re-render when a state in an item is changed.
    This benefit will be noticed when the lists start to accept props for its data.

---
+++ readable/src/components/PostItem/PostItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import './PostItem.css'
+import {
+  MdDelete,
+  MdEdit,
+  MdArrowDropUp,
+  MdArrowDropDown,
+  MdPerson
+} from 'react-icons/lib/md'
+
+class PostItem extends Component {
+  render() {
+    return (
+      <div className="post-item">
+        <div className="post-update-button-group">
+          <MdEdit className="post-update-button" />
+          <MdDelete className="post-update-button" />
+        </div>
+        <div className="post-vote-info-block">
+          <div className="post-vote-function">
+            <div className="post-vote-button-group">
+              <MdArrowDropUp className="post-vote-button" />
+              <span>12</span>
+              <MdArrowDropDown className="post-vote-button" />
+            </div>
+          </div>
+          <div className="post-info">
+            <h3 className="post-title">This is the the titleThThis is the the titleThis is the the title</h3>
+            <span className="post-detail">
+              <span>Posted by: </span>
+              <MdPerson className="profile-image" />
+              {/* <img src="http://via.placeholder.com/20x20" alt="Name Profile" className="profile-image" /> */}
+              <span className="dot-separator">&#9679;</span>
+              <span>17 comments</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PostItem
