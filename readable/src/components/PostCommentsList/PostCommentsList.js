ce3b7c6b5a4e71fa7286b0c76271356252d8db2d
Ging Gonzalez <ginggonzalez3@gmail.com>
2017-09-07 12:56:41 -0700

    refactor: Separate Comments Lists
    PostCommentsList is now in its own component that takes a post as a prop.

---
+++ readable/src/components/PostCommentsList/PostCommentsList.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import {
+  toggleDeleteCommentConfirmation,
+  toggleEditCommentForm,
+} from '../../actions/forms'
+import {
+  fetchCommentsByPostId,
+  deleteCommentByIdRequest,
+  voteCommentRequest,
+} from '../../actions/comments'
+import EditButton from '../EditButton/EditButton'
+import DeleteButton from '../DeleteButton/DeleteButton'
+import Modal from '../Modal/Modal'
+import DeleteConfirm from '../DeleteConfirm/DeleteConfirm'
+import VotingCircle from '../VotingCircle/VotingCircle'
+import AddCommentForm from '../AddCommentForm/AddCommentForm'
+
+import './PostCommentsList.css'
+
+class PostCommentsList extends Component {
+
+  shouldComponentUpdate = nextProps => {
+    return JSON.stringify(this.props) !== JSON.stringify(nextProps)
+  } 
+
+  confirmDeleteComment = (comment) => {
+    this.props.toggleDeleteCommentConfirmation(comment)
+    this.props.deleteCommentByIdRequest(comment.id)
+  }
+
+  // This function contains functions to be called in render() that will cause
+  // the component to re-render
+  // These functions should be assisted by shouldComponentUpdate() react lifecycle
+  stateChangersInRender = () => {
+    this.props.fetchCommentsByPostId(this.props.post.id)
+  }
+
+  render() {
+    const post = this.props.openedPost
+    const comments = this.props.openedPostComments
+
+    this.stateChangersInRender()
+
+    return (
+      <div id="post-item-comments-container">
+        {comments && comments.map(comment =>
+          <div id="post-item-comment" key={comment.id}>
+            <div className="pid-post-update-button-group">
+              <EditButton editButtonCallBack={() => this.props.toggleEditCommentForm(comment)} />
+              <DeleteButton deleteButtonCallBack={() => this.props.toggleDeleteCommentConfirmation(comment)} />
+            </div>
+            <div id="pic-comment-details">
+              <div id="pic-comment-voting-circle-container">
+                <VotingCircle
+                  id="pic-comment-voting-circle"
+                  voteScore={comment.voteScore}
+                  upVoteCallback={() => this.props.voteCommentRequest(comment.id, "upVote")}
+                  downVoteCallback={() => this.props.voteCommentRequest(comment.id, "downVote")}
+                />
+              </div>
+              <span id="pic-comment-right">
+                <span id="pic-comment-author">{comment.author}</span>
+                <span id="pic-comment-comment">{comment.body}</span>
+              </span>
+            </div>
+            <Modal
+              isOpen={this.props.isDeleteCommentConfirmationOpen && comment.id === (this.props.editingComment && this.props.editingComment.id)}
+              closeModalCallback={() => this.props.toggleDeleteCommentConfirmation(comment)}>
+              <DeleteConfirm
+                cancelCallback={() => this.props.toggleDeleteCommentConfirmation(comment)}
+                confirmCallback={() => this.confirmDeleteComment(comment)} />
+            </Modal>
+            {this.props.isEditCommentFormOpen && comment.id === (this.props.editingComment && this.props.editingComment.id) && <AddCommentForm post={post} comment={comment} />}
+          </div>)}
+        {this.props.isEditCommentFormOpen && !this.props.editingComment && <AddCommentForm post={post} />}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ posts, forms, comments }) => {
+  return {
+    openedPost: posts.openedPost,
+    isEditCommentFormOpen: forms.isEditCommentFormOpen,
+    editingComment: forms.editingComment,
+    openedPostComments: comments.openedPostComments,
+    isDeleteCommentConfirmationOpen: forms.isDeleteCommentConfirmationOpen,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCommentsByPostId: (id) => dispatch(fetchCommentsByPostId(id)),
+    voteCommentRequest: (id, option) => dispatch(voteCommentRequest(id, option)),
+    toggleEditCommentForm: (comment) => dispatch(toggleEditCommentForm(comment)),
+    deleteCommentByIdRequest: (id) => dispatch(deleteCommentByIdRequest(id)),
+    toggleDeleteCommentConfirmation: (comment) => dispatch(toggleDeleteCommentConfirmation(comment)),
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PostCommentsList))
--- readable/src/components/PostItemDetailPage/PostItemDetailPage.js
+++ readable/src/components/PostItemDetailPage/PostItemDetailPage.js
@@ -7,15 +7,9 @@ import {
 } from '../../actions/posts'
 import {
   toggleDeletePostConfirmation,
-  toggleDeleteCommentConfirmation,
   toggleEditPostForm,
   toggleEditCommentForm,
 } from '../../actions/forms'
-import {
-  fetchCommentsByPostId,
-  deleteCommentByIdRequest,
-  voteCommentRequest,
-} from '../../actions/comments'
 import Timestamp from 'react-timestamp'
 import {
   MdReply,
@@ -26,7 +20,7 @@ import Modal from '../Modal/Modal'
 import AddPostForm from '../AddPostForm/AddPostForm'
 import DeleteConfirm from '../DeleteConfirm/DeleteConfirm'
 import VotingCircle from '../VotingCircle/VotingCircle'
-import AddCommentForm from '../AddCommentForm/AddCommentForm'
+import PostCommentsList from '../PostCommentsList/PostCommentsList'
 
 import './PostItemDetailPage.css'
 
@@ -34,23 +28,16 @@ class PostItemDetailPage extends Component {
 
   componentWillMount = () => {
     this.props.openPostRequest(this.props.match.params.post_id)
-    this.props.fetchCommentsByPostId(this.props.match.params.post_id)
   }
 
   confirmDelete = (post) => {
     this.props.toggleDeletePostConfirmation(post)
     this.props.deletePostRequest(post.id)
-    this.props.history.goBack()
-  }
-
-  confirmDeleteComment = (comment) => {
-    this.props.toggleDeleteCommentConfirmation(comment)
-    this.props.deleteCommentByIdRequest(comment.id)
+    this.props.history.push(`/${post.category}`)
   }
 
   render() {
     const post = this.props.openedPost
-    const comments = this.props.openedPostComments
     return (
       <div id="post-item-detail-page">
         <div id="post-item-detail">
@@ -69,9 +56,9 @@ class PostItemDetailPage extends Component {
               <div id="pid-voting-circle-container">
                 <VotingCircle
                   id="pid-voting-circle"
-                  voteScore={post.voteScore}
-                  upVoteCallback={() => this.props.voteRequest(post.id, "upVote")}
-                  downVoteCallback={() => this.props.voteRequest(post.id, "downVote")}
+                  voteScore={post && post.voteScore}
+                  upVoteCallback={() => this.props.voteRequest(post && post.id, "upVote")}
+                  downVoteCallback={() => this.props.voteRequest(post && post.id, "downVote")}
                 />
               </div>
             </div>
@@ -84,39 +71,7 @@ class PostItemDetailPage extends Component {
               />
             </div>
           </div>
-          {/* Comments section */}
-          <div id="post-item-comments-container">
-            {comments && comments.map(comment =>
-              <div id="post-item-comment" key={comment.id}>
-                <div className="pid-post-update-button-group">
-                  <EditButton editButtonCallBack={() => this.props.toggleEditCommentForm(comment)} />
-                  <DeleteButton deleteButtonCallBack={() => this.props.toggleDeleteCommentConfirmation(comment)} />
-                </div>
-                <div id="pic-comment-details">
-                  <div id="pic-comment-voting-circle-container">
-                    <VotingCircle
-                      id="pic-comment-voting-circle"
-                      voteScore={comment.voteScore}
-                      upVoteCallback={() => this.props.voteCommentRequest(comment.id, "upVote")}
-                      downVoteCallback={() => this.props.voteCommentRequest(comment.id, "downVote")}
-                    />
-                  </div>
-                  <span id="pic-comment-right">
-                    <span id="pic-comment-author">{comment.author}</span>
-                    <span id="pic-comment-comment">{comment.body}</span>
-                  </span>
-                </div>
-                <Modal
-                  isOpen={this.props.isDeleteCommentConfirmationOpen && comment.id === (this.props.editingComment && this.props.editingComment.id)}
-                  closeModalCallback={() => this.props.toggleDeleteCommentConfirmation(comment)}>
-                  <DeleteConfirm
-                    cancelCallback={() => this.props.toggleDeleteCommentConfirmation(comment)}
-                    confirmCallback={() => this.confirmDeleteComment(comment)} />
-                </Modal>
-                {this.props.isEditCommentFormOpen && comment.id === (this.props.editingComment && this.props.editingComment.id) && <AddCommentForm post={post} comment={comment} />}
-              </div>)}
-            {this.props.isEditCommentFormOpen && !this.props.editingComment && <AddCommentForm post={post} />}
-          </div>
+          <PostCommentsList post={post && post}/>
         </div>
         <Modal
           isOpen={this.props.isDeleteConfirmationOpen}
@@ -128,7 +83,7 @@ class PostItemDetailPage extends Component {
         <Modal
           isOpen={this.props.isEditPostFormOpen && post.id === this.props.editingPost.id}
           closeModalCallback={() => this.props.toggleEditPostForm(undefined)}>
-          <AddPostForm id={post.id} />
+          <AddPostForm id={post && post.id} />
         </Modal>
       </div>
     )
@@ -138,13 +93,9 @@ class PostItemDetailPage extends Component {
 const mapStateToProps = ({ posts, forms, comments }) => {
   return {
     openedPost: posts.openedPost,
-    isEditCommentFormOpen: forms.isEditCommentFormOpen,
     isDeleteConfirmationOpen: forms.isDeleteConfirmationOpen,
     isEditPostFormOpen: forms.isEditPostFormOpen,
     editingPost: forms.editingPost,
-    editingComment: forms.editingComment,
-    openedPostComments: comments.openedPostComments,
-    isDeleteCommentConfirmationOpen: forms.isDeleteCommentConfirmationOpen,
   }
 }
 
@@ -154,12 +105,8 @@ const mapDispatchToProps = dispatch => {
     deletePostRequest: (id) => dispatch(deletePostRequest(id)),
     toggleDeletePostConfirmation: (id) => dispatch(toggleDeletePostConfirmation(id)),
     toggleEditPostForm: (post) => dispatch(toggleEditPostForm(post)),
-    fetchCommentsByPostId: (id) => dispatch(fetchCommentsByPostId(id)),
     voteRequest: (id, option) => dispatch(voteRequest(id, option)),
-    voteCommentRequest: (id, option) => dispatch(voteCommentRequest(id, option)),
     toggleEditCommentForm: (comment) => dispatch(toggleEditCommentForm(comment)),
-    deleteCommentByIdRequest: (id) => dispatch(deleteCommentByIdRequest(id)),
-    toggleDeleteCommentConfirmation: (comment) => dispatch(toggleDeleteCommentConfirmation(comment)),
   }
 }
 
