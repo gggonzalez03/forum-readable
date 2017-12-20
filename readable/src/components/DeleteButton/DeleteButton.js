7dffdc1721d012c5c2bcfe953116a54529d65062
Ging Gonzalez <ginggonzalez3@gmail.com>
2017-12-20 09:12:16 -0800

    publish: Prepare repository for publish
    This prepares the backend server to be published first on heroku.
    The front-end code is removed temporarily to acheive this goal.

--- readable/src/components/DeleteButton/DeleteButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import MdDelete from 'react-icons/lib/md/delete'
-import './DeleteButton.css'
-
-const DeleteButton = ({deleteButtonCallBack}) => (
-  <MdDelete id="dpb-delete-button" onClick={() => deleteButtonCallBack()}/>
-)
-
-export default DeleteButton
